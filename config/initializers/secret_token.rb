06a6b066d54a3dd5b99f45497976a0733204b7c2
Bishwa Hang Rai <bishwahang.kirat@gmail.com>
2013-12-11 23:51:12 +0100

    Addded contact page

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,25 +1,3 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-# Be sure to restart your server when you modify this file.
-
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
 require 'securerandom'
 
 def secure_token
@@ -34,4 +12,4 @@ def secure_token
     token
   end
 end
-SampleApp::Application.config.secret_key_base = '720769bc6d25ba087f3f3a4f5b0e9ce207300fc3c2c34541ac041f37b079391a1ddc6edbfd4dd4e8bd8b9caf04b205aa0dc3050af3d60fd28680a810a12c6d03'
+SampleApp::Application.config.secret_token = secure_token
